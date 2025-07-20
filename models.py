f00dc35918cb5b33ecef5986e0b53a6205827f89
jedaqsaul <jedaqsaul@gmail.com>
2025-07-20 23:30:50 +0300

    add naming conventions

--- models.py
+++ models.py
@@ -4,3 +4,11 @@ from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
 import re
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
