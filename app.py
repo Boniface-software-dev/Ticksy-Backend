f535c5f64948ad2d7e9171b7b44456aa34299778
jedaqsaul <jedaqsaul@gmail.com>
2025-07-20 23:15:30 +0300

    add app configuration

--- app.py
+++ app.py
@@ -10,4 +10,11 @@ from datetime import timedelta
 
 load_dotenv()
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///development.db"  # dev only
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = "your_jwt_secret"  # temp secret
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
+
+
+
 
