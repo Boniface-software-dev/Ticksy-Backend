009803e3de6f52a1ecd6d5c4852a805aad37c906
g_zawie <gzawie24@gmail.com>
2025-07-20 22:51:05 +0300

    Add user model

--- models.py
+++ models.py
@@ -4,3 +4,37 @@ from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
 import re
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
+
+# ------------------ User ------------------
+class User(db.Model, SerializerMixin):
+    _tablename_ = "users"
+    serialize_rules = (
+        "-password",
+        "-events.organizer",
+        "-orders.attendee",
+        "-reviews.attendee",
+        "-saved_events.user",
+        "-logs.user",
+        "-reports.admin"
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(20), default="attendee")  # attendee, organizer, admin
+    status = db.Column(db.String(20), default="active")
+    created_at = db.Column(db.DateTime, default=datetime.now)
