10c73870fcdc1cec973690e1c1b6c22f67c1bcf2
g_zawie <gzawie24@gmail.com>
2025-07-21 20:59:46 +0300

    Add status parser

--- resources/admin_users.py
+++ resources/admin_users.py
@@ -3,3 +3,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import request
 from models import User, db
 from utils.logger import log_action
+
+status_parser = reqparse.RequestParser()
+status_parser.add_argument("status", type=str, required=True)  # "active" or "banned"
+
