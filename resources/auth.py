b5caf1e5e1b05f382b8b4510641468726f661242
jedaqsaul <jedaqsaul@gmail.com>
2025-07-21 20:02:48 +0300

    add login and sign up request parsers

--- resources/auth.py
+++ resources/auth.py
@@ -6,3 +6,16 @@ from utils.logger import log_action
 from datetime import timedelta
 import bcrypt  # BCRYPT USED HERE
 import re
+
+
+signup_parser = reqparse.RequestParser()
+signup_parser.add_argument("first_name", type=str, required=True)
+signup_parser.add_argument("last_name", type=str, required=True)
+signup_parser.add_argument("email", type=str, required=True)
+signup_parser.add_argument("phone", type=str, required=True)
+signup_parser.add_argument("password", type=str, required=True)
+signup_parser.add_argument("role", type=str, required=True)
+
+login_parser = reqparse.RequestParser()
+login_parser.add_argument("email", type=str, required=True)
+login_parser.add_argument("password", type=str, required=True)
